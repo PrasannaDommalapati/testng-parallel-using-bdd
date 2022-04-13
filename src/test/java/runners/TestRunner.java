f0d3e21f4d777c59e3acc310737c17822f9c8d02
Prasanna Dommalapati <prasanna.dommalapati@zonal.co.uk>
2022-04-13 15:36:20 +0100

    config added

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -3,6 +3,9 @@ package runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(tags = "", features = "src/main/resources/features", glue = "stepDefinitions")
+@CucumberOptions(
+        tags = "",
+        features = "src/main/resources/features",
+        glue = "stepDefinitions")
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
